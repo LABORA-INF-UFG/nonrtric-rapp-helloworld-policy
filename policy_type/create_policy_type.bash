ada70f4b3211a8d44367ef2cf6d2a29763429b77
zanattabruno <gkbsa2504@gmail.com>
2023-07-13 21:38:17 +0000

    Added policy type creator script

---
+++ policy_type/create_policy_type.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+curl -v -X PUT http://service-ricplt-a1mediator-http.ricplt.svc.cluster.local:10000/a1-p/policytypes/2 \
+-H "Content-Type: application/json" \
+-d '{
+    "name": "hwpolicy",
+    "description": "Hellow World policy type",
+    "policy_type_id": 2,
+    "create_schema": {
+      "$schema": "http://json-schema.org/draft-07/schema#",
+      "title": "HW Policy",
+      "description": "Hello World Policy Type",
+      "type": "object",
+      "properties": {
+        "threshold": {
+          "type": "integer",
+          "default": 0
+        }
+      },
+      "additionalProperties": false
+    }
+  }'
